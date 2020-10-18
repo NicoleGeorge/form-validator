37f935798c72dda04cd571b64b56f86d32bc8600
AdorkablyNicole <hello@nicolegeorge.me>
2020-10-18 19:25:27 +1030

    FV stage 2- input error message and event listener added

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// INPUT ERROR MESSAGE
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+}
+
+// EVENT LISTENERS
+
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  console.log('submit');
+  //   console.log('submit'); working!!
+  //   console.log(username.value); woot-woot
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
 });
